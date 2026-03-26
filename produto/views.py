bcdf06c647cbbfdae843c7bc9d3562936187de6a
BrunoBiazon <brunobiazon10@gmail.com>
2026-03-26 15:16:07 -0300

    Começando a implementar o carrinho no produto views

--- produto/views.py
+++ produto/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
 
 class ListaProdutos(ListView):
@@ -17,7 +18,35 @@ class Detalheproduto(DetailView):
     slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
-    pass
+    def get(self, request, *args, **kwargs): 
+        http_referer = request.META.get(
+            'HTTP_REFERER',
+             reverse('produto:lista')                                
+        )
+        id_prod = request.GET.get('vid')
+        
+        if not id_prod:
+            messages.error(
+                request, 'Produto não existe'
+            )
+            return redirect(http_referer) 
+            
+            variacao = get_object_or_404(models.Variacao, id= variacao_id)
+            
+            if not self.request.seesion.get('carrinho'):
+                self.request.session['carrinho'] = {}
+                self.request.session.save()
+                
+            carrinho = self.request.session['carrinho']
+            
+            if variacao_id in carrinho:
+                TODO
+                pass
+            else:   
+                TODO
+            
+        return redirect(http_referer)
+
 class RemoverDoCarrinho(View):
     pass
 
