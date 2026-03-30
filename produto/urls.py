a5c8af8ced6b8819611de18612bf43f4db168d7a
BrunoBiazon <brunobiazon10@gmail.com>
2026-03-30 01:06:40 -0300

    Correção urls com a adiação de novos paths, caso o cliente venha a comprar por meio do link das categorias.

--- produto/urls.py
+++ produto/urls.py
@@ -4,13 +4,14 @@ from . import views
 app_name = 'produto'
 
 urlpatterns = [
-    path('', views.ListaProdutos.as_view(), name= "lista"),
-    path('<slug>', views.Detalheproduto.as_view(), name= "detalhe"),
-    path('adiconaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name= "adicionaraocarrinho"),
-    path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name= "removerdocarrinho"),
-    path('carrinho/', views.Carrinho.as_view(), name= "carrinho"),
-    path('finalizar/', views.Finalizar.as_view(), name= "finalizar"),
-    path('promocao/', views.Promocao.as_view(), name= "promocao"),
+    path('', views.ListaProdutos.as_view(), name="lista"),
+
+    path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name="adicionaraocarrinho"),
+    path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name="removerdocarrinho"),
+    path('carrinho/', views.Carrinho.as_view(), name="carrinho"),
+    path('finalizar/', views.Finalizar.as_view(), name="finalizar"),
+
+    path('promocao/', views.Promocao.as_view(), name="promocao"),
     path('snacks/', views.Snacks.as_view(), name="snacks"),
     path('whey/', views.Whey.as_view(), name="whey"),
     path('saude/', views.Saude.as_view(), name="saude"),
@@ -19,4 +20,18 @@ urlpatterns = [
     path('termo/', views.Termo.as_view(), name="termo"),
     path('hiper/', views.Hiper.as_view(), name="hiper"),
     path('endurance/', views.Endurance.as_view(), name="endurance"),
-    ]   
+
+    path('snacks/<slug>', views.Detalheproduto.as_view(), name="detalhe_snack"),
+    path('whey/<slug>', views.Detalheproduto.as_view(), name="detalhe_whey"),
+    path('saude/<slug>', views.Detalheproduto.as_view(), name="detalhe_saude"),
+    path('creatina/<slug>', views.Detalheproduto.as_view(), name="detalhe_creatina"),
+    path('pretreino/<slug>', views.Detalheproduto.as_view(), name="detalhe_pretreino"),
+    path('termo/<slug>', views.Detalheproduto.as_view(), name="detalhe_termo"),
+    path('hiper/<slug>', views.Detalheproduto.as_view(), name="detalhe_hiper"),
+    path('endurance/<slug>', views.Detalheproduto.as_view(), name="detalhe_endurance"),
+
+    #path('perfil/', views.Perfil.as_view(), name="perfil"),
+    #path('pedido/', views.Pedido.as_view(), name="pedido"),
+
+    path('<slug>', views.Detalheproduto.as_view(), name="detalhe"),
+]
