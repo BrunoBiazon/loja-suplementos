62ef8941ed9fde18d647cdd9218ad820885212f3
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-01 17:56:09 -0300

    Feat: label confirmação de senha, Fix: def clean, teste validações

--- perfil/forms.py
+++ perfil/forms.py
@@ -10,11 +10,39 @@ class PerfilForm(forms.ModelForm):
         exclude = ('user',)
 
 class UserForm(forms.ModelForm):
+    password = forms.CharField(
+        required= False,
+        widget= forms.PasswordInput(),
+        label = 'Senha'
+    )
+    
+    password2 = forms.CharField(
+        required= False,
+        widget= forms.PasswordInput(),
+        label = 'Confirme a senha'
+    )
+    
+    
+    def __init__(self, user=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+
     class Meta:
         model = User
-        fields = ['first_name','last_name','username', 'password','email']
-        
-    def clean(self,*args, **kwargs):
+        fields = ('first_name', 'last_name', 'username', 'password', 'password2','email')
+
+    def clean(self, *args, **kwargs):
         data = self.data 
-        cleaned = self.clean_field
-    pass 
+        cleaned = self.cleaned_data
+        validation_error_msgs = {}
+        
+        
+        if self.user:
+            validation_error_msgs['username'] = 'teste logado '
+        else:
+            validation_error_msgs['username'] = 'teste '
+                
+        if validation_error_msgs:
+            raise(forms.ValidationError(validation_error_msgs))
+        
+        return cleaned
