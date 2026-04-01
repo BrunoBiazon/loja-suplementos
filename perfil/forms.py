1ecf6d80e3d1f278a3f77b3d747201545c03cc91
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-01 19:11:16 -0300

    Feat: tratamento usuário deslogado

--- perfil/forms.py
+++ perfil/forms.py
@@ -36,11 +36,38 @@ class UserForm(forms.ModelForm):
         cleaned = self.cleaned_data
         validation_error_msgs = {}
         
+        usuario_data = cleaned.get('username')
+        email_data = cleaned.get('email')
+        password_data = cleaned.get('password')
+        password2_data = cleaned.get('password')
         
+        usuario_db = User.objects.filter(username=usuario_data).first()
+        email_db = User.objects.filter(email=email_data).first()
+        
+        error_msg_user_exists = 'Usuário digitado já existe.'
+        error_msg_email_exists = 'Email digitado já existe'        
+        error_msg_password_match = 'As senhas não conferem. Digite iguais.'       
+        error_msg_password_short = 'Sua senha precisa de pelo menos 8 caracteres.'
+        error_msg_required_filed = 'Preencha esse campo.'
+        
+        # user logado - update
         if self.user:
             validation_error_msgs['username'] = 'teste logado '
+            
+            
+        # user deslogado
         else:
-            validation_error_msgs['username'] = 'teste '
+            if usuario_db:
+                validation_error_msgs['username'] = error_msg_user_exists
+                
+            if email_db:
+                validation_error_msgs['email'] = error_msg_email_exists
+                
+            if password_data != password_data2:
+                validation_error_msgs['password'] = error_msg_password_match
+                validation_error_msgs['password_confirm'] = error_msg_password_match
+            elif len(password_data) < 8:
+                validation_error_msgs['password'] = error_msg_password_short    
                 
         if validation_error_msgs:
             raise(forms.ValidationError(validation_error_msgs))
