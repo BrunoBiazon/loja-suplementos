d1660a97db25487ab51dcc743ff852f6569de702
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-02 01:09:07 -0300

    Feat: Impletação da view Criar | Feat: Tratamento de CPF na class PerfilForm por meio da função clean, não permite utilizar um CPF já cadastrado
     View Criar:
    
    -def get: redireciona para o template lista caso o usuário ja esteja logado e tente acessar o cadastro (/perfil/);
    
    -Metodo Post: Faz a verificação se os 2 forms são válidos, com isso, faz tratamento de  segurança para o user não ter permissões de admin. Por fim, salva o cadastro e emite  messages.sucess redirecionando para a lista de produtos ('/');

--- perfil/forms.py
+++ perfil/forms.py
@@ -9,7 +9,27 @@ class PerfilForm(forms.ModelForm):
         fields = '__all__'
         exclude = ('user',)
 
+    def clean(self, *args, **kwargs):
+            cleaned = super().clean()
+            validation_error_msgs = {}
+            
+            cpf_data = cleaned.get('cpf')
+            
+            if cpf_data:
+                cpf_db = models.PerfilUsuario.objects.filter(cpf=cpf_data).first()
+                
+                perfil_id = self.instance.pk 
+                if cpf_db:
+                    if perfil_id is None or cpf_db.pk != perfil_id:
+                        validation_error_msgs['cpf'] = 'Este CPF já está cadastrado em outra conta.'  
+                    
+            if validation_error_msgs:    
+                raise forms.ValidationError(validation_error_msgs)
+            
+            return cleaned
+    
 class UserForm(forms.ModelForm):
+    
     password = forms.CharField(
         required= False,
         widget= forms.PasswordInput(),
@@ -31,14 +51,13 @@ class UserForm(forms.ModelForm):
         fields = ('first_name', 'last_name', 'username', 'password', 'password2','email')
 
     def clean(self, *args, **kwargs):
-        data = self.data 
         cleaned = self.cleaned_data
         validation_error_msgs = {}
         
         usuario_data = cleaned.get('username')
         email_data = cleaned.get('email')
         password_data = cleaned.get('password')
-        password2_data = cleaned.get('password')
+        password2_data = cleaned.get('password2')
         
         usuario_db = User.objects.filter(username=usuario_data).first()
         email_db = User.objects.filter(email=email_data).first()
@@ -55,14 +74,14 @@ class UserForm(forms.ModelForm):
                 validation_error_msgs['username'] = error_msg_user_exists
             if email_db and email_db.id != self.user.id:
                 validation_error_msgs['email'] = error_msg_email_exists
-            
+                            
             if password_data or password2_data:
                 if not password_data or not password2_data:
                     validation_error_msgs['password'] = error_msg_required_field
-                    validation_error_msgs['password_confirm'] = error_msg_required_field
+                    validation_error_msgs['password2'] = error_msg_required_field
                 elif password_data != password2_data:
                     validation_error_msgs['password'] = error_msg_password_match
-                    validation_error_msgs['password_confirm'] = error_msg_password_match
+                    validation_error_msgs['password2'] = error_msg_password_match
                 elif len(password_data) < 8:
                     validation_error_msgs['password'] = error_msg_password_short
 
@@ -75,10 +94,10 @@ class UserForm(forms.ModelForm):
             
             if not password_data or not password2_data:
                 validation_error_msgs['password'] = error_msg_required_field
-                validation_error_msgs['password_confirm'] = error_msg_required_field
+                validation_error_msgs['password2'] = error_msg_required_field
             elif password_data != password2_data:
                 validation_error_msgs['password'] = error_msg_password_match
-                validation_error_msgs['password_confirm'] = error_msg_password_match
+                validation_error_msgs['password2'] = error_msg_password_match
             elif len(password_data) < 8:
                 validation_error_msgs['password'] = error_msg_password_short
                 
--- perfil/views.py
+++ perfil/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views import View
 
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
 
@@ -12,7 +13,9 @@ class BasePerfil(View):
     template_name = 'perfil/criar.html'
 
     def setup(self, *args, **kwargs):
+        
         super().setup(*args, **kwargs)
+
         
         data = self.request.POST or None
 
@@ -20,7 +23,7 @@ class BasePerfil(View):
             self.contexto = {
                 'userform': forms.UserForm(
                     data=data,
-                    usuario=self.request.user,  
+                    user=self.request.user,  
                     instance=self.request.user   
                 ),
                 'perfilform': forms.PerfilForm(data=data)
@@ -38,7 +41,42 @@ class BasePerfil(View):
     def get(self, *args, **kwargs):
         return self.renderizar
 class Criar(BasePerfil):
-    def post(self,*args,**kwargs):
+    
+    def get(self, *args, **kwargs): # caso ja tenha conta, não consegue acessar cadastro
+        if self.request.user.is_authenticated:
+            return redirect('/') 
+        return super().get(*args, **kwargs)
+    
+    def post(self, *args, **kwargs):
+        userform = self.contexto['userform']
+        perfilform = self.contexto['perfilform']
+
+        if userform.is_valid() and perfilform.is_valid():
+            
+            usuario = userform.save(commit=False) 
+            
+            usuario.is_staff = False
+            usuario.is_superuser = False
+            
+            senha = userform.cleaned_data.get('password')
+            usuario.set_password(senha)
+            
+            usuario.save() 
+        
+            perfil = perfilform.save(commit=False)
+            perfil.user = usuario
+            perfil.save() 
+            
+            print('Perfil salvo teste')
+            
+            messages.success(self.request, "Usuário Cadastrado com sucesso.")
+            return redirect('/')
+            
+        else:
+            print('Perfil não salvo teste - Erro de Validação')
+            
+            messages.error(self.request, "Não foi possível efetuar o cadastro.")
+        
         return self.renderizar
 
 class Update(BasePerfil):
