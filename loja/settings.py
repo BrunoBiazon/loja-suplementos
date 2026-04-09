755a2551f8a78bd1f949059a8ff77847e0184682
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-09 01:52:55 -0300

    feat: test settings white noise

--- loja/settings.py
+++ loja/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'    
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
