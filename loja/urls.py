5429a70102476ac6075376071d26d3c04d726089
BrunoBiazon <brunobiazon10@gmail.com>
2026-04-09 03:16:48 -0300

    feat: troquei white noise por nginx, para leitura da imagens
    -algumas imagens ainda continuam não funcionando na aws

--- loja/urls.py
+++ loja/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('perfil/', include('perfil.urls')),
     path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
